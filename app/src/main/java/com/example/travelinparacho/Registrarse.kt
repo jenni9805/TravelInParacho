3b25674427258bebff9059b40234b0843426406b
jenni9805 <58237071+jenni9805@users.noreply.github.com>
2020-05-19 13:38:31 -0500

    Initial commit

---
+++ app/src/main/java/com/example/travelinparacho/Registrarse.kt
@@ -0,0 +1,12 @@
+package com.example.travelinparacho
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Registrarse : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registrarse)
+    }
+}
