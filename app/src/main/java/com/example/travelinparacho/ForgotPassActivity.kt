090911e94f61a52923d084a5c4b5a75cffb7b9c8
RamiroMendoza16 <58205350+RamiroMendoza16@users.noreply.github.com>
2020-05-21 18:36:12 -0500

    agregar recuperar contraseña

---
+++ app/src/main/java/com/example/travelinparacho/ForgotPassActivity.kt
@@ -0,0 +1,44 @@
+package com.example.travelinparacho
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class ForgotPassActivity : AppCompatActivity() {
+
+    private lateinit var txtEmail: EditText
+    private lateinit var auth: FirebaseAuth
+    private lateinit var progressBar: ProgressBar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_pass)
+        txtEmail=findViewById(R.id.txtEmail)
+        auth= FirebaseAuth.getInstance()
+        progressBar = findViewById(R.id.progressBar)
+
+    }
+
+    fun send(view:View){
+
+        val email=txtEmail.text.toString()
+        if (!TextUtils.isEmpty(email)){
+            auth.sendPasswordResetEmail(email)
+                .addOnCompleteListener(this){
+                    task ->
+                    if (task.isSuccessful){
+                        progressBar.visibility=View.VISIBLE
+                        startActivity(Intent(this,MainActivity::class.java))
+                    }else{
+                        Toast.makeText(this, "Error al enviar el Email", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
+    }
+}
