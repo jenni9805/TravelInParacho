87daf996a694e057aac21797a463e1a25a9e0464
jenni9805 <58237071+jenni9805@users.noreply.github.com>
2020-05-21 01:23:08 -0500

    fondohome y cambios de pagina

--- app/src/main/java/com/example/travelinparacho/Registrarse.kt
+++ app/src/main/java/com/example/travelinparacho/Registrarse.kt
@@ -1,12 +1,19 @@
 package com.example.travelinparacho
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_registrarse.*
 
 class Registrarse : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrarse)
+
+        cuenta.setOnClickListener(){
+            val intent= Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
