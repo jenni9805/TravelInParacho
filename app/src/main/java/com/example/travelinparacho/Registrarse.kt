03114e52ccf15a9c0332ff5b65d1966e507763ea
jenni9805 <58237071+jenni9805@users.noreply.github.com>
2020-05-21 11:07:13 -0500

    Cambios en el archivo Registrarse.kt
    Error de creacion de funciones modificado

--- app/src/main/java/com/example/travelinparacho/Registrarse.kt
+++ app/src/main/java/com/example/travelinparacho/Registrarse.kt
@@ -19,9 +19,9 @@ import java.lang.ref.Reference
 class Registrarse : AppCompatActivity() {
 
     private lateinit var txtName: EditText
+    private lateinit var txtLastName: EditText
     private lateinit var txtEmail: EditText
     private lateinit var txtPass: EditText
-    private lateinit var txtLastname: EditText
     private lateinit var progressBar: ProgressBar
     private lateinit var dbReference: DatabaseReference
     private lateinit var database: FirebaseDatabase
@@ -31,67 +31,68 @@ class Registrarse : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrarse)
         txtName=findViewById(R.id.txtName)
+        txtLastName=findViewById(R.id.txtLastName)
         txtEmail=findViewById(R.id.txtEmail)
         txtPass=findViewById(R.id.txtPass)
-        txtLastname=findViewById(R.id.txtLastname)
 
 
+        //progressBar = ProgressBar(this)
         progressBar = findViewById(R.id.progressBar)
 
         database= FirebaseDatabase.getInstance()
         auth= FirebaseAuth.getInstance()
         dbReference=database.reference.child("User")
 
-
-        fun verifyEmail(user:FirebaseUser?){
-            user?.sendEmailVerification()
-                ?.addOnCompleteListener(this){
-                        task ->
-                    if (task.isComplete){
-                        Toast.makeText(this, "Email enviado", Toast.LENGTH_LONG).show()
-                    }else{
-                        Toast.makeText(this, "Error al enviar el correo", Toast.LENGTH_LONG).show()
-                    }
-                }
-        }
-
-        fun action(){
-            startActivity(Intent(this, MainActivity::class.java))
-        }
-
-         fun createNewAccount(){
-            val name:String=txtName.text.toString()
-            val email:String=txtEmail.text.toString()
-            val pass:String=txtPass.text.toString()
-            val lastName:String=txtLastname.text.toString()
-
-            if(!TextUtils.isEmpty(name) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(pass) && !TextUtils.isEmpty(lastName) ){
-                progressBar.visibility=View.VISIBLE
-                
-                auth.createUserWithEmailAndPassword(email, pass)
-                    .addOnCompleteListener(this) {
-                        task ->
-                        if (task.isComplete){
-                            val user:FirebaseUser?=auth.currentUser
-                            verifyEmail(user)
-
-                            val userBD=dbReference.child(user?.uid.toString())
-
-                            userBD.child("Name").setValue(name)
-                            userBD.child("LastName").setValue(lastName)
-                            action()
-                        }
-                    }
-            }
-        }
-
-        fun register(view:View) {
-            createNewAccount()
-        }
-
         cuenta.setOnClickListener(){
             val intent= Intent(this,MainActivity::class.java)
             startActivity(intent)
         }
     }
+
+     private fun verifyEmail(user:FirebaseUser?){
+        user?.sendEmailVerification()
+            ?.addOnCompleteListener(this){
+                    task ->
+                if (task.isComplete){
+                    Toast.makeText(this, "Email enviado", Toast.LENGTH_LONG).show()
+                }else{
+                    Toast.makeText(this, "Error al enviar el correo", Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    private fun action(){
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+
+    private fun createNewAccount(){
+        val name:String=txtName.text.toString()
+        val lastName:String=txtLastName.text.toString()
+        val email:String=txtEmail.text.toString()
+        val pass:String=txtPass.text.toString()
+
+        if(!TextUtils.isEmpty(name) && !TextUtils.isEmpty(lastName) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(pass)){
+            progressBar.visibility=View.VISIBLE
+
+            auth.createUserWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(this) {
+                        task ->
+                    if (task.isComplete){
+                        val user:FirebaseUser?=auth.currentUser
+                        verifyEmail(user)
+
+                        val userBD=dbReference.child(user?.uid.toString())
+
+                        userBD.child("Name").setValue(name)
+                        userBD.child("LastName").setValue(lastName)
+                        action()
+                    }
+                }
+        }
+    }
+
+    fun registro(view:View) {
+        createNewAccount()
+    }
+
 }
